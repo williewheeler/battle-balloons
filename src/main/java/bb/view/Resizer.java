1924da7972f29d7bb832ec7181a4e48923788503
Willie Wheeler <willie.wheeler@gmail.com>
2017-06-07 02:15:18 -0700

    Cleaned up the resizer by removing unnecessary BufferedImage code

--- src/main/java/bb/view/Resizer.java
+++ src/main/java/bb/view/Resizer.java
@@ -7,52 +7,34 @@ import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
-import java.awt.image.BufferedImage;
 
-import static bb.BBConfig.*;
+import static bb.BBConfig.SCREEN_HEIGHT_PX;
+import static bb.BBConfig.SCREEN_WIDTH_PX;
 
 /**
  * See https://stackoverflow.com/questions/44363464/scale-tiny-low-resolution-app-to-larger-screen-size
- * 
+ *
  * Created by willie on 6/7/17.
  */
 public class Resizer extends JPanel {
 	private static final int K = 3;
-	private static final Dimension K_SCREEN_SIZE_PX = new Dimension(K * SCREEN_WIDTH_PX, K * SCREEN_HEIGHT_PX);
-	private static final AffineTransform SCALE_TRANSFORM;
-
-	static {
-		SCALE_TRANSFORM = new AffineTransform();
-		SCALE_TRANSFORM.scale(K, K);
-	}
+	private static final Dimension PREF_SIZE = new Dimension(K * SCREEN_WIDTH_PX, K * SCREEN_HEIGHT_PX);
+	private static final AffineTransform SCALE_XFORM = AffineTransform.getScaleInstance(K, K);
 
 	public Resizer(JComponent component) {
-
-		// This basically shrinkwraps the child component.
 		setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
-
 		add(component);
 	}
 
 	@Override
 	public Dimension getPreferredSize() {
-		return K_SCREEN_SIZE_PX;
+		return PREF_SIZE;
 	}
 
 	@Override
-	public void paintComponent(Graphics g) {
+	public void paint(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
-		BufferedImage image = getImage();
-		System.out.println(image);
-		g2.setTransform(SCALE_TRANSFORM);
-		g2.drawImage(image, 0, 0, SCREEN_WIDTH_PX, SCREEN_HEIGHT_PX, null);
-	}
-
-	private BufferedImage getImage() {
-		BufferedImage image = new BufferedImage(SCREEN_WIDTH_PX, SCREEN_HEIGHT_PX, BufferedImage.TYPE_INT_ARGB);
-		Graphics2D g2 = image.createGraphics();
-		super.paintComponent(g2);
-		g2.dispose();
-		return image;
+		g2.setTransform(SCALE_XFORM);
+		super.paint(g2);
 	}
 }
