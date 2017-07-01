f3cdbe8050794e8241d2a35f5b960e1ec9f38b15
Willie Wheeler <willie.wheeler@gmail.com>
2017-07-01 12:28:45 -0700

    Added package documentation to the core packages.

---
+++ src/main/java/bb/attract/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for attract mode screens, which are screens designed to entice players into playing. They appeared especially
+ * in arcade games.
+ *
+ * Created by willie on 7/1/17.
+ */
+package bb.attract;
