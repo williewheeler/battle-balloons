ef7e9c8e7f0ded052d91ca00eea4dcf415af2a99
Willie Wheeler <willie.wheeler@gmail.com>
2017-07-06 00:37:36 -0700

    Removed delay

--- src/main/java/bb/BB.java
+++ src/main/java/bb/BB.java
@@ -51,12 +51,6 @@ public class BB extends JFrame {
 		setLocationRelativeTo(null);
 		setVisible(true);
 
-		try {
-			Thread.sleep(20000);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
-
 		stateMachine.start();
 	}
 
