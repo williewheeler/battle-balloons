b270b49ceaa1d7b330dc7b7a058c2947ece22ae0
Willie Wheeler <wwheeler@gmail.com>
2017-07-15 13:18:33 -0400

    Balloons splat on impact

--- src/main/java/com/williewheeler/bb/common/actor/model/Balloon.java
+++ src/main/java/com/williewheeler/bb/common/actor/model/Balloon.java
@@ -10,9 +10,12 @@ import static com.williewheeler.retroge.GameConfig.WORLD_SIZE;
  */
 public class Balloon extends AbstractActor {
 	public static final int SPEED = 5;
+	public static final int EXIT_TTL = 7;
 
 	private static final int WIDTH = 3;
 	private static final int HEIGHT = 3;
+	
+	private int exitTtl = EXIT_TTL;
 	
 	// TODO This mechanism is different than the direction mechanism.
 	// Choose one or the other. (I kind of like dx, dy better.) [WLW]
@@ -23,6 +26,10 @@ public class Balloon extends AbstractActor {
 		super(null, x, y, WIDTH, HEIGHT);
 		this.dx = dx;
 		this.dy = dy;
+	}
+	
+	public int getExitTtl() {
+		return exitTtl;
 	}
 	
 	public int getDx() {
@@ -39,15 +46,22 @@ public class Balloon extends AbstractActor {
 		changeY(dy);
 		checkGC();
 	}
-
-	// TODO Exiting should make a splash if it hits somebody, but not if it just goes off-screen.
-
+	
+	@Override
+	public void updateBodyExiting() {
+		this.exitTtl--;
+		if (exitTtl <= 0) {
+			this.exitTtl = EXIT_TTL;
+			setState(ActorLifecycleState.GONE);
+		}
+	}
+	
 	private void checkGC() {
 		final int x = getX();
 		final int y = getY();
 
 		if (x < 0 || x > WORLD_SIZE.width || y < 0 || y > WORLD_SIZE.height) {
-			setState(ActorLifecycleState.GONE);
+			setState(ActorLifecycleState.EXITING);
 		}
 	}
 }
